ce1ab8a5fd3c3ac24fb4e6bf009139108b8a879f
mouWorks <mou.wang@g.ncu.edu.tw>
2019-10-02 16:17:13 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'MouWorks blog',
+  title: 'MouWorks',
   description: 'MouWorks',
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
