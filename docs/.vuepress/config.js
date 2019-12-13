8420c1e8894a0faa8da889169aead8635a7646c4
mouWorks <mou.wang@g.ncu.edu.tw>
2019-12-13 11:05:37 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     nav:[ // 导航栏配置
       {text: 'DevOps', link: '/devops/',
         items: [
-          { text: 'Deployment', link: '/devops/deployment.html'},
+          { text: 'Deployment', link: '/devops/deployment_questions.html'},
           { text: 'Linux', link: '/devops/linux.html'},
           { text: 'CircleCI', link: '/devops/circleci.html'},
           { text: 'Ansible', link: '/devops/ansible.html'},
